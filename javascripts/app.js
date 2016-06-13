1543f60e0fdd1ac46cd02de09bf618cecd14aa2b
Jacob Fowles <js.fowles@gmail.com>
2016-06-13 16:14:39 -0600

    working version

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,5 +1,30 @@
 function getInputs(){
-  var inputOne = parseFloat(document.getElementById('num1').value);
-  var inputTwo = parseFloat(document.getElementById('num2').value);
-	
+  var inputOne = parseFloat(document.getElementById('input_1').value);
+  var inputTwo = parseFloat(document.getElementById('input_2').value);
+  return [inputOne, inputTwo];
+}
+
+var math = 0
+function operator(choice){
+  math = choice;
+}
+
+function calculate(){
+  var inputs = getInputs();
+  if (math == 1){
+    var output = inputs[0] + inputs[1];
+  }
+  else if (math == 2){
+    var output = inputs[0] - inputs[1];
+  }
+  else if (math == 3){
+    var output = inputs[0] * inputs[1];
+  }
+  else if (math == 4){
+    var output = inputs[0] / inputs[1];
+  }
+  else{
+    var  output = "Select operator"
+  }
+  document.getElementById('output').innerHTML = output;
 }
